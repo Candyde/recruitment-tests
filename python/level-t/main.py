1f7ccc97a82084d4bcbe453c18315dd6150cee40
Candyde <nabyl.bc@gmail.com>
2021-09-10 19:29:43 +0200

    Update main.py
    palindrome in progress

--- python/level-t/main.py
+++ python/level-t/main.py
@@ -1,2 +1,13 @@
+
 def is_palindrome(string):
-    pass
+    reversed_string = string[::-1]
+    status=1
+    if(string!=reversed_string):
+        status=0
+     
+    string = input("Enter the string: ")
+    status= is_palindrome(string)
+    if(status):
+        return("It is a palindrome ")
+    else:
+        return("Sorry! Try again")
