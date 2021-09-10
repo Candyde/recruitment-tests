d367e9f4358a3804823f61007207ca54f11a47e2
Candyde <nabyl.bc@gmail.com>
2021-09-10 19:22:40 +0200

    fizzbuzz done

--- python/level-p/main.py
+++ python/level-p/main.py
@@ -1,2 +1,14 @@
 def fizzbuzz(n):
-    pass
+    if n <= 0 : 
+        return []
+    for i in range(1,101):
+        variable = ''
+        if i % 3 == 0 and n % 5 == 0:
+            return 'FizzBuzz'
+        elif i % 3 == 0:
+            return 'Fizz'
+        elif i % 5 == 0:
+            return 'Buzz'
+        else:
+            return str(n)
+
